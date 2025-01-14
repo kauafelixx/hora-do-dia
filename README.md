6287206cac24ca96f278bd1791fe29c87c9d1f23
kauafelixx <161839119+kauafelixx@users.noreply.github.com>
2025-01-14 14:16:02 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# hora-do-dia
+ Projeto que muda de estilo de acordo com o horário do dia
---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+function carregar() {
+    var msg = document.getElementById('msg')
+    var imagem = document.getElementById('imagem')
+
+    var data= new Date()
+    var hora= 1
+
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    if (hora >= 0 && hora <12){
+        //bom dia
+        imagem.src="img/manha.png"
+        document.body.style.background ='#f9d487'
+        document.h1.style.color='#000000'
+           document.p.style.color='#000000'
+    }else if (hora >=12 && hora <18){
+        //boa tarde
+        imagem.src="img/tarde.png"
+            document.body.style.background ='#f08925'
+    }else{
+        //boa noite
+        imagem.src="img/noite.png"
+            document.body.style.background ='#3E7786'
+        
+    }
+}
